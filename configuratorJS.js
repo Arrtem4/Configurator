166c3d72550a6d5f44f189877a697982970c5929
Artem <yakovenko.art1@gmail.com>
2023-12-14 11:49:38 +0500

    first

---
+++ configuratorJS.js
@@ -0,0 +1,28 @@
+$(function () {
+    // variables
+    let area = $(".configurator-area");
+    let menuItems = $(".menu-item");
+    let selections = $(".selection");
+
+    // actions
+    $(document).click(function (event) {
+        if (!$(event.target).closest("#configurator-area").length) {
+            $("#configurator-area").find(".active").removeClass("active");
+        }
+    });
+
+    menuItems.on("click", function () {
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            return area
+                .find(`[name=${$(this).data("name")}]`)
+                .removeClass("active");
+        }
+        menuItems.removeClass("active");
+        selections.removeClass("active");
+        $(this).addClass("active");
+        area.find(`[name=${$(this).data("name")}]`).addClass("active");
+    });
+
+    // changes
+});
