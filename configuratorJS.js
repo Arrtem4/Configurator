a31ebfc4a8a1ac19bee5129eda85425c1e0fdcdc
Artem <yakovenko.art1@gmail.com>
2023-12-27 16:49:09 +0500

    angles

--- configuratorJS.js
+++ configuratorJS.js
@@ -4,15 +4,25 @@ $(function () {
     let menuItems = $(".configurator-area .menu-item");
     let selections = $(".configurator-area .selection");
     let buttonModeArr = $(".configurator-area .button-mode");
+    let buttonStyleArr = $(".configurator-area .button-angles-style");
     let buttonFacadeMaterialArr = $(
         ".configurator-area .button-material-facade"
     );
     let buttonPlinthMaterialArr = $(
         ".configurator-area .button-material-plinth"
     );
+    let buttonFramingMaterialArr = $(
+        ".configurator-area .button-material-framing"
+    );
+    let buttonAnglesMaterialArr = $(
+        ".configurator-area .button-material-angles"
+    );
     let colorFacadeArr = $(".configurator-area .color-facade");
     let colorPlinthArr = $(".configurator-area .color-plinth");
+    let colorFramingArr = $(".configurator-area .color-framing");
+    let colorAnglesArr = $(".configurator-area .color-angles");
     let fullscreen = false;
+    let anglesStyleMode = 0;
     // actions
     $(document).click(function (event) {
         if (!$(event.target).closest("#configurator-area").length) {
@@ -36,6 +46,7 @@ $(function () {
         buttonModeArr.removeClass("active");
         $(this).addClass("active");
     });
+
     // material mode
     $(".configurator-area .button-material-facade").on("click", function () {
         buttonFacadeMaterialArr.removeClass("active");
@@ -53,6 +64,24 @@ $(function () {
             "active"
         );
     });
+    $(".configurator-area .button-material-framing").on("click", function () {
+        buttonFramingMaterialArr.removeClass("active");
+        colorFramingArr.removeClass("active");
+        $(this).addClass("active");
+        area.find(`[name=framing-palette-${$(this).data("value")}]`).addClass(
+            "active"
+        );
+    });
+    $(".configurator-area .button-material-angles").on("click", function () {
+        console.log($(this).data("value"));
+        buttonAnglesMaterialArr.removeClass("active");
+        colorAnglesArr.removeClass("active");
+        $(this).addClass("active");
+        area.find(`[name=angles-palette-${$(this).data("value")}]`).addClass(
+            "active"
+        );
+    });
+
     // color mode
     $(".configurator-area .color-item-facade").on("click", function () {
         $(".configurator-area .color-item-facade").removeClass("selected");
@@ -74,16 +103,22 @@ $(function () {
         selections.removeClass("active");
         menuItems.removeClass("active");
     });
+    // $(".configurator-area .color-item-framing").on("click", function () {
+    //     $(".configurator-area .color-item-framing").removeClass("selected");
+    //     $(this).addClass("selected");
+    //     $(".configurator-area .layer-framing").prop(
+    //         "src",
+    //         `${$(this).data("value")}`
+    //     );
+    //     selections.removeClass("active");
+    //     menuItems.removeClass("active");
+    // });
     $(".configurator-area .color-item-angles").on("click", function () {
         $(".configurator-area .color-item-angles").removeClass("selected");
         $(this).addClass("selected");
-        if ($(this).data("value") === "none") {
-            $(".configurator-area .layer-angles").addClass("disabled");
-        } else {
-            $(".configurator-area .layer-angles")
-                .removeClass("disabled")
-                .prop("src", `${$(this).data("value")}`);
-        }
+        $(".configurator-area .layer-angles")
+            .removeClass("disabled")
+            .prop("src", `${$(this).data("value")}`);
         selections.removeClass("active");
         menuItems.removeClass("active");
     });
@@ -97,6 +132,21 @@ $(function () {
         selections.removeClass("active");
         menuItems.removeClass("active");
     });
+    // style-angles mode
+    $(".configurator-area .button-angles-style").on("click", function () {
+        buttonStyleArr.removeClass("active");
+        $(this).addClass("active");
+        if ($(this).data("value") === 0) {
+            selections.removeClass("active");
+            menuItems.removeClass("active");
+            $(".configurator-area .angles-style-hidden").removeClass("show");
+            return $(".configurator-area .layer-angles").addClass("disabled");
+        } else {
+            $(".configurator-area .angles-style-hidden").addClass("show");
+            return (anglesStyleMode = $(this).data("value"));
+        }
+    });
+
     // fullscreen button
     $(".fullscreen-button").on("click", function () {
         let fullscreenButton = $("#fullscreenButton");
